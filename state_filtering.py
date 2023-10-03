602ca5fb13f273d984bbfaaf9d938480ea085439
Isabelle M. Vea <isabelle.vea@gmail.com>
2023-10-03 16:58:43 -0500

    state_filtering
    Working and census files directly downloadable

--- state_filtering.py
+++ state_filtering.py
@@ -2,9 +2,51 @@ import pandas as pd
 import geopandas as gpd
 import pyarrow
 import sys
+import requests
+import os
+import zipfile
+
+
+def download_census():
+
+	print('Downloading Census zip file from data.gov')
+	zip_url = "https://www2.census.gov/geo/tiger/TIGER2019/COUNTY/tl_2019_us_county.zip"
+	local_zip_file = "tl_2019_us_county.zip"
+
+	if not os.path.exists(local_zip_file):
+		print(f"Downloading {zip_url}...")
+		response = requests.get(zip_url)
+	
+		if response.status_code == 200:
+			with open(local_zip_file, 'wb') as zip_file:
+				zip_file.write(response.content)
+			print(f"Downloaded {local_zip_file} successfully.")
+		else:
+			print(f"Failed to download {zip_url}. Status code: {response.status_code}")
+	else:
+		print(f"{local_zip_file} already exists. Skipping download.")
+
+
+	# Path to the downloaded ZIP file
+	zip_file_path = "tl_2019_us_county.zip"
+
+	# Directory where you want to extract the contents
+	extracted_dir = "tl_2019_us_county"
+
+	# Check if the extracted directory already exists; if not, unzip the file
+	if not os.path.exists(extracted_dir):
+		print(f"Extracting {zip_file_path} to {extracted_dir}...")
+		with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+			zip_ref.extractall(extracted_dir)
+		print(f"Extracted {zip_file_path} successfully to {extracted_dir}.")
+	else:
+		print(f"{extracted_dir} already exists. Skipping extraction.")
+
+
 
 def open_shapefile_state():
-
+	download_census()
+	
 	print('Opening census shapefile dataset')
 
 	gdf = gpd.read_file('tl_2019_us_county/tl_2019_us_county.shp') #file is too large to commit; download from https://catalog.data.gov/dataset/tiger-line-shapefile-2019-nation-u-s-current-county-and-equivalent-national-shapefile
